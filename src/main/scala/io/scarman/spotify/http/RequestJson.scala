7d601de8f56a53d370a08908525a0da65301908e
Stephen Carman <shcarman@gmail.com>
2017-10-01 13:34:32 -0400

    Removed a testing method that I could just do myself where needed.

--- src/main/scala/io/scarman/spotify/http/RequestJson.scala
+++ src/main/scala/io/scarman/spotify/http/RequestJson.scala
@@ -1,9 +1,7 @@
 package io.scarman.spotify.http
 
 import org.json4s.JValue
-import org.json4s.jackson.JsonMethods._
 
 private[spotify] trait RequestJson {
-  private[http] var json: Option[JValue]                = None
-  protected[spotify] def responseJson(): Option[String] = json.map(pretty)
+  private[http] var json: Option[JValue] = None
 }
